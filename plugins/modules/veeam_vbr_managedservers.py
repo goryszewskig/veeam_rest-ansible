8effcda15c6aee327381179adee23fd49add045a
= <markus.kraus@gmail.com>
2020-11-10 00:01:41 +0100

    init veeam_vbr_managedservers

---
+++ plugins/modules/veeam_vbr_managedservers.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2020, Your Name <YourName@example.org>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: veeam_vbr_managedservers
+
+short_description: Add Veeam Backup & Replication Managed Servers.
+
+version_added: "1.0.0"
+
+description: Add Veeam Backup & Replication Managed Servers.
+
+options:
+    validate_certs:
+        description: Validate SSL certs.
+        required: false
+        default: false
+        type: bool
+    server_name:
+        description: VBR Server Name or IP
+        required: true
+        type: str
+    server_port:
+        description: VBR RestAPI Sever Port
+        required: false
+        default: 9419
+        type: str
+    server_username:
+        description: VBR Server Username
+        required: true
+        type: str
+    server_password:
+        description: VBR Server password
+        required: true
+        type: str
+
+author:
+    - Markus Kraus (@vMarkusK)
+'''
+
+EXAMPLES = r'''
+# Pass in a message
+
+'''
+
+import json
+import re
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.urls import fetch_url
+
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        server_name=dict(type='str', required=True),
+        server_username=dict(type='str', required=True),
+        server_password=dict(type='str', required=True, no_log=True),
+        server_port=dict(type='str', default='9419'),
+        validate_certs=dict(type='bool', default='false'),
+        state=dict(type="str", choices=("absent", "present"), default="present"),
+        name=dict(type='str', required=True),
+        servertype=dict(type='str', chuies=("ViHost", "WindowsHost"), required=True),
+        credentials_id=dict(type='str', required=True)
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False
+    )
+
+    # General
+    state = module.params['state']
+
+    # Authenticate
+    request_server = module.params['server_name']
+    request_port = module.params['server_port']
+    request_username = module.params['server_username']
+    request_password = module.params['server_password']
+    payload = 'grant_type=password&username=' + request_username + '&password=' + request_password
+    headers = {
+        'accept': 'application/json',
+        'x-api-version': '1.0-rev1',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'true'
+    }
+
+    request_url = 'https://' + request_server + ':' + request_port + '/api/oauth2/token'
+
+    method = "Post"
+    req, info = fetch_url(module, request_url, headers=headers, method=method, data=payload)
+
+    if info['status'] != 200:
+        module.fail_json(msg="Fail: %s" % ("Status: " + str(info['status']) + ", Message: " + str(info['msg'])))
+
+    try:
+        resp = json.loads(req.read())
+    except AttributeError:
+        module.fail_json(msg='Parsing Response Failed', **result)
+
+    # Payload
+    if state == 'present':
+        name = module.params['name']
+        servertype = module.params['servertype']
+        credentialsId = module.params['credentials_id']
+
+        body = {
+            'name': name,
+            'type': servertype,
+            'credentialsId': credentialsId
+        }
+        bodyjson = json.dumps(body)
+        headers = {
+            'x-api-version': '1.0-rev1',
+            'Authorization': 'Bearer ' + resp['access_token'],
+            'Content-Type': 'application/json'
+        }
+        request_url = 'https://' + request_server + ':' + request_port + '/api/v1/backupInfrastructure/managedServers'
+
+        method = "Post"
+        req, info = fetch_url(module, request_url, headers=headers, method=method, data=bodyjson)
+
+        if info['status'] != 200:
+            module.fail_json(msg="Fail: %s" % ("Status: " + str(info['status']) + ", Message: " + str(info['msg'])))
+
+        try:
+            result['msg'] = json.loads(req.read())
+            result['changed'] = True
+        except AttributeError:
+            module.fail_json(msg='Parsing Response Failed', **result)
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
